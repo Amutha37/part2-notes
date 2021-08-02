34993674f117797cf87cabd8f9cdf2941c5b8702
Amutha Muhunthan <amuthamuhunthan@Amuthas-iMac.local>
2021-08-02 15:54:59 +1000

    seperate all HTTP GET,PUT AND POST in on modules and use cleaner syntax for defining object literals.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react'
 import Note from './components/Note';
- import axios from 'axios';
+import noteService from './services/notes'
 
 const App = () => {
   const [notes, setNotes] = useState([])
@@ -9,13 +9,11 @@ const App = () => {
 
 
   useEffect(() => {
-
-    const eventHandler = response => {  
-      setNotes(response.data)
-    }
-  
-    const promise = axios.get('http://localhost:3001/notes')
-    promise.then(eventHandler)
+    noteService
+    .getAll()
+    .then(initialNotes => {
+      setNotes(initialNotes)
+    })
   }, [])
 
   
@@ -28,13 +26,12 @@ const App = () => {
       important: Math.random() > 0.5,
       id: notes.length + 1,
     }
-    axios
-    .post('http://localhost:3001/notes', noteObject)
-    .then(response => {
-    
-      setNotes(notes.concat(response.data))
-    setNewNote('')
-    })
+    noteService
+      .create(noteObject)
+      .then(returnedNote => {
+        setNotes(notes.concat(returnedNote))
+        setNewNote('')
+      })
   }
 
   const handleNoteChange = (event) => {
@@ -56,13 +53,15 @@ const App = () => {
 // new note is then sent with a PUT request to the backend where it will replace the old object. put(url, changedNote)
     // callback function sets the state and render component notes with new array , except for the old note with is replaced with teh note exact item.
    
-    const url = `http://localhost:3001/notes/${id}`
     const note = notes.find(n => n.id === id)
     const changedNote = { ...note, important: !note.important }
    
-  axios.put(url, changedNote).then(response => {
-    setNotes(notes.map(note => note.id !== id ? note : response.data))
-  })
+    noteService
+      .update(id, changedNote)
+      .then(returnedNote => {
+        setNotes(notes.map(note => note.id !== id ? note : returnedNote))
+      })
+  
   })
       
       
